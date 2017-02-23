3628b85ed2cf1c73deb61ed11d46ebe4ccd91ec4
masklinn <github.com@masklinn.net>
2017-02-23 10:41:24 +0100

    Complexify but speed up Rust version

--- yes.rs
+++ yes.rs
@@ -1,11 +1,14 @@
 use std::env;
+use std::io::{self, Write};
 
 fn main() {
     let args: Vec<String> = env::args().collect();
     let args = &args[1..];
 
     let output = if args.is_empty() { "y".to_string() } else { args.join(" ") };
+    let stdout = io::stdout();
+    let mut out = stdout.lock();
     loop {
-        println!("{}", output)
+        let _ = out.write(output.as_bytes());
     }
 }
